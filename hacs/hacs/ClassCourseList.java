6dfdd4be33336d6590b8a1aa5d03169bfa9a1c28
Jay Chiddarwar <jchiddar@asu.edu>
2022-10-30 15:53:29 -0700

    implemented java style guides in ClassCourseList.java

--- hacs/hacs/ClassCourseList.java
+++ hacs/hacs/ClassCourseList.java
@@ -5,44 +5,42 @@ import java.io.*;
 
 /**
  * Title: HACS Description: Copyright: Copyright (c) 2002 Company: msu
- * 
+ *
  * @author Zhang ji Zhu Wei
- * @version 1.0
  * @author mjfindler
  * @version 2.0 update to Java 8
  */
 
 public class ClassCourseList extends ArrayList<Course> {
 
-	public ClassCourseList() {
-	}
+    public ClassCourseList() {
+    }
 
 
-	void InitializeFromFile(String theFileName) {
-		try {
-			BufferedReader file;
-			String strCourseName = null;
-			file = new BufferedReader(new FileReader("CourseInfo.txt"));
-			while ((strCourseName = file.readLine()) != null) {
-				Course theCourse;
-				theCourse = new Course(strCourseName, 0);
-//      theCourse.CourseName= strCourseName;
-				add(theCourse);
-			}
-		} catch (Exception ee) {
-			;
-		}
-	}
+    void initializeFromFile(String theFileName) {
+        try {
+            BufferedReader file;
+            String strCourseName = null;
+            file = new BufferedReader(new FileReader("CourseInfo.txt"));
+            while ((strCourseName = file.readLine()) != null) {
+                Course theCourse;
+                theCourse = new Course(strCourseName, 0);
+                add(theCourse);
+            }
+        } catch (Exception ee) {
+            System.err.println(ee);
+        }
+    }
 
-	Course FindCourseByCourseName(String CourseName) {
-		int nCourseCount = size();
-		for (int i = 0; i < nCourseCount; i++) {
-			Course theCourse;
-			theCourse = (Course) get(i);
-			if (theCourse.CourseName.compareTo(CourseName) == 0)
-				return theCourse;
-		}
-		return null;
-	}
+    Course findCourseByCourseName(String courseName) {
+        int nCourseCount = size();
+        for (int i = 0; i < nCourseCount; i++) {
+            Course theCourse;
+            theCourse = (Course) get(i);
+            if (theCourse.CourseName.compareTo(courseName) == 0)
+                return theCourse;
+        }
+        return null;
+    }
 
 }
