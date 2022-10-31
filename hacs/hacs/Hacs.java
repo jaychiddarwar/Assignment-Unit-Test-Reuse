5e43de3e7cfe51bbf0a20e856c86a77b3a656938
Jay Chiddarwar <jchiddar@asu.edu>
2022-10-31 13:08:38 -0700

    code cleanup on Hacs.java

--- hacs/hacs/Hacs.java
+++ hacs/hacs/Hacs.java
@@ -2,46 +2,37 @@ package hacs;
 
 /**
  * Title: HACS Description: Copyright: Copyright (c) 2002 Company: msu
- * 
+ *
  * @author Zhang ji Zhu Wei
- * @version 1.0
  * @author mjfindler
  * @version 2.0
- * 
+ * <p>
  * Update to Java 8
  */
 
 public class Hacs {
+    static Facade theFacade = new Facade();
 
-	static Facade theFacade = new Facade();
-
-	public Hacs() {
-	}
-
-	public static void main(String[] args) {
-//		String strUsername;
-//		String strUserType = null;
-		UserInfoItem userinfoitem = new UserInfoItem();
-		theFacade.createCourseList();
-		while (true) {
-			boolean bExit = false;
-			bExit = theFacade.Login(userinfoitem);
-			if (bExit)
-				break;
-			// userinfoitem.strUserName = "Inst1";
-			// userinfoitem.UserType = 1;
-			theFacade.createUser(userinfoitem);
-			theFacade.attachCourseToUser();
-			if (userinfoitem.UserType == UserInfoItem.USER_TYPE.Student) // if is a student remind him of the due date
-				theFacade.remind();
-			boolean bLogout = false;
-			while (!bLogout) {
-				bLogout = theFacade.selectCourse();
-				if (bLogout)
-					break;
-				bLogout = theFacade.courseOperation();
-			}
-		}
-//    System.out.println(userinfoitem.strUserName +userinfoitem.UserType );
-	}
+    public static void main(String[] args) {
+        UserInfoItem userInfoItem = new UserInfoItem();
+        theFacade.createCourseList();
+        while (true) {
+            boolean bExit = false;
+            bExit = theFacade.Login(userInfoItem);
+            if (bExit) {
+                break;
+            }
+            theFacade.createUser(userInfoItem);
+            theFacade.attachCourseToUser();
+            if (userInfoItem.UserType == UserInfoItem.USER_TYPE.Student)
+                theFacade.remind();
+            boolean bLogout = false;
+            while (!bLogout) {
+                bLogout = theFacade.selectCourse();
+                if (bLogout)
+                    break;
+                bLogout = theFacade.courseOperation();
+            }
+        }
+    }
 }
